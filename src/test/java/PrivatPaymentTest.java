8f0368d0bf754e4a9e6999420b810a967c40667a
didya <didyalife@gmail.com>
2022-01-27 09:36:03 +0200

    remove Thread.sleep change Assertions check to shouldHave

--- src/test/java/PrivatPaymentTest.java
+++ src/test/java/PrivatPaymentTest.java
@@ -1,4 +1,5 @@
 
+import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Selenide;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.Assert;
@@ -8,11 +9,10 @@ import org.openqa.selenium.By;
 public class PrivatPaymentTest {
 
     @Test
-    public void testPrivatPayment() throws InterruptedException {
-        String payerCard = "4552331448138217";
-        String reciverCard = "4004159115449003";
+    public void testPrivatPayment() {
+        String payerCard = "4552 3314 4813 8217";
+        String reciverCard = "4004 1591 1544 9003";
         String paymentAmount = "123";
-        String paymentCurrency = "USD";
         WebDriverManager.chromedriver().setup();
 
         Selenide.open("https://next.privat24.ua/money-transfer/card");
@@ -27,22 +27,16 @@ public class PrivatPaymentTest {
 
         Selenide.$(By.xpath("//button[@data-qa-node=\"currency\"]")).click();
         Selenide.$(By.xpath("//button[@data-qa-value=\"USD\"]")).click();
-        Thread.sleep(1000);
 
         Selenide.$(By.xpath("//button[@type=\"submit\"]")).click();
-        String currentPayerCard = Selenide.$(By.xpath("//span[@data-qa-node=\"payer-card\"]"))
-                .text().replaceAll("\\s", "");
-        String currentReciverCard = Selenide.$(By.xpath("//span[@data-qa-node=\"receiver-card\"]"))
-                .text().replaceAll("\\s", "");
-        String currentPayerAmount = Selenide.$(By.xpath("//div[@data-qa-node=\"payer-amount\"]"))
-                .text().replaceAll("\\s", "");
-        String currentReciverAmount = Selenide.$(By.xpath("//div[@data-qa-node=\"receiver-amount\"]"))
-                .text().replaceAll("\\s", "");
 
-        Assert.assertTrue("Text not valid !", currentPayerCard.contains(payerCard));
-        Assert.assertTrue("Text not valid !", currentReciverCard.contains(reciverCard));
-        Assert.assertTrue("Text not valid !", currentPayerAmount.contains(paymentAmount + paymentCurrency));
-        Assert.assertTrue("Text not valid !", currentReciverAmount.contains(paymentAmount + paymentCurrency));
-
+        Selenide.$(By.xpath("//span[@data-qa-node=\"payer-card\"]"))
+                .shouldHave(Condition.text(payerCard));
+        Selenide.$(By.xpath("//span[@data-qa-node=\"receiver-card\"]"))
+                .shouldHave(Condition.text(reciverCard));
+        Selenide.$(By.xpath("//div[@data-qa-node=\"payer-amount\"]"))
+                .shouldHave(Condition.text(paymentAmount));
+        Selenide.$(By.xpath("//div[@data-qa-node=\"receiver-amount\"]"))
+                .shouldHave(Condition.text(paymentAmount));
     }
 }
